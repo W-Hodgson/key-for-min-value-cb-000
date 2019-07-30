10564968dbfe7c976a05d3b7ba815d85a93ceddf
Will Hodgson <will.hodgson@current-rms.com>
2019-07-30 09:00:18 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,9 +3,13 @@
 
 def key_for_min_value(name_hash)
   prices = name_hash.collect do |name, price|
-    price
+    lowest_price = price
   end
-  puts prices
+  i = 0
+  while i < price.length
+
+
+  end
 end
 
 key_for_min_value({one: 100, two: 20, three: 500})
