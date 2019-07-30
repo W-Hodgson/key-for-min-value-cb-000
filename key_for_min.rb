54cf5400221326880d9f762949b2b69dcf8ed98c
Will Hodgson <will.hodgson@current-rms.com>
2019-07-30 09:04:05 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -9,7 +9,9 @@ def key_for_min_value(name_hash)
   current_lowest = 0
   while i < prices.length
     current_lowest = prices[i]
-
+    if prices[i+1] < prices[i]
+      current_lowest = prices[i+1]
+    end
   end
 end
 
